d5041875d132c35e64ab62ce6dbf5a3c6a69b1f9
Théodorebinda <theodorebinda@gmail.com>
2024-07-29 13:11:27 +0100

    Add search input, integrate button component, and implement scroll area 🤸🏾‍♀️

--- src/app/(main-route)/page.tsx
+++ src/app/(main-route)/page.tsx
@@ -1,12 +1,20 @@
 'use client'
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Container } from '@/ui/components/container/container';
+import { Icon, Search } from 'lucide-react';
 import React from 'react';
 
 
 const App: React.FC = () => {
   return (
-    <main className='text-center pt-16'>
-      <h1>Hello</h1>
-    </main>
+    <Container className='text-center pt-16'>
+      <Container className='flex'>
+        <Input type='search'  placeholder='search'/>
+        <Button className='-z-3' size={"icon"}><Search/></Button>
+      </Container>
+
+    </Container>
   );
 };
 
