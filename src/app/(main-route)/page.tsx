3cc810393767799822a954c52dc42680e765f49a
Théodorebinda <theodorebinda@gmail.com>
2024-07-29 19:45:37 +0100

    add aspect ratio for image

--- src/app/(main-route)/page.tsx
+++ src/app/(main-route)/page.tsx
@@ -1,11 +1,5 @@
 'use client'
 import Header from '@/components/Header';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { Container } from '@/ui/components/container/container';
-import { Typography } from '@/ui/components/typography/typography';
-import { ScrollTagArt } from '@/ui/modules/scrollTagArticle/scrollTagArticle';
-import { Search } from 'lucide-react';
 import React from 'react';
 
 
@@ -13,6 +7,7 @@ const App: React.FC = () => {
   return (
     <div className='pt-20 '>
        <Header /> 
+       
     </div>
   );
 };
